c34d3fee351b3abd3625cca924da7b1779e0791a
sisayj <87474568+sisayj@users.noreply.github.com>
2024-03-06 15:22:49 -0500

    Delete webapp directory

--- webapp/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-def home(request):
-    context = {
-        'title': 'Dynamic Web Page',
-        'content': 'Welcome to our dynamic web page!'
-    }
-    return render(request, 'home.html', context)
-
